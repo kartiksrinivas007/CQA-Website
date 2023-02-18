c41fa3cd0492bf95e3e3737e4c2735796bcfc513
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2023-02-18 19:16:24 +0530

    Add Detail post view

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', show_posts.as_view(), name='show_posts')
+    path('', show_posts.as_view(), name='show_posts'),
+    path('<int:pk>/', detail_post.as_view(), name='detail_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import *
 
 # app_name = 'posts'
@@ -20,5 +21,17 @@ class show_posts(ListView):
     
     def get_query_set(self, *args, **kwargs):
         qs = super(show_posts, self).get_query_set(*args, **kwargs)
+        print(qs)
+        return qs
+
+
+class detail_post(DetailView):
+    template_name = 'posts/detail_post.html'
+    model = Posts
+    ordering = ['-creation_date']
+    context_object_name = 'post'
+    
+    def get_context_data(self, *args, **kwargs):
+        qs = super(detail_post, self).get_context_data(*args, **kwargs)
         print(qs)
         return qs
